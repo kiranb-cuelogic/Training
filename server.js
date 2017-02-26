26f2ffb81ae920c9d7a5ae8eb59c7a0ab1647c7a
kiranb-cuelogic <kiran.bangale@cuelogic.co.in>
2017-02-26 19:15:29 +0530

    SSL and CORS implementation.

--- server.js
+++ server.js
@@ -1,26 +1,46 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+const express = require('express');
+const https = require('https');
+const fs = require('fs');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const io = require('socket.io')(https);
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+// const port = process.env.PORT;
+const options = {
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem')
+};
+dotenv.load();
+
+
+const app = express();
+const whitelist = ['http://example1.com', 'http://example2.com']
+
+const corsOptionsDelegate = function (req, callback) {
+  var corsOptions;
+  if (whitelist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+  }else{
+    corsOptions = { origin: false } // disable CORS for this request
+  }
+  callback(null, corsOptions) // callback expects two parameters: error and options
+}
+
+app.get('/', cors(corsOptionsDelegate), function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for a whitelisted domain.'})
+})
+
+const server = https.createServer(options, (req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World!!!');
 });
 
 io.on('connection', function(socket){
-
-  console.log('a user connected');
-
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
-    io.emit('chat message', msg);
-  });
-
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
-
+	socket.on('event', function(data){});
+  	socket.on('disconnect', function(){});
+  	console.log('a user connected');
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+server.listen(process.env.PORT, function(){
+  	console.log('CORS-enabled web server listening on :' + process.env.PORT);
 });
